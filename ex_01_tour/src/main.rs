cb7fbf6db9c22932151811e28ee0439ddf471ab4
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-06-19 04:04:07 +0700

    Add ex 01

---
+++ ex_01_tour/src/main.rs
@@ -0,0 +1,12 @@
+// a comment
+fn main() {
+    say_hello();
+
+    println!("\nThis is the end of main")
+}
+
+fn say_hello() {
+    println!("\nEntering function say_hello");
+    println!("\nHello, world!");
+    println!("\nLeaving function say_hello");
+}

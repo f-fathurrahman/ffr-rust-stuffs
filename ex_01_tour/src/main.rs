3c18d802f61da8c5961a7282176b742a88fbf266
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-06-19 04:28:47 +0700

    Add gcd, test test

--- ex_01_tour/src/main.rs
+++ ex_01_tour/src/main.rs
@@ -1,6 +1,9 @@
+mod gcd;
+
 // a comment
 fn main() {
     say_hello();
+    driver_gcd();
 
     println!("\nThis is the end of main")
 }
@@ -9,4 +12,10 @@ fn say_hello() {
     println!("\nEntering function say_hello");
     println!("\nHello, world!");
     println!("\nLeaving function say_hello");
+}
+
+fn driver_gcd() {
+    println!("\nEntering function driver_gcd");
+    println!("\ngcd(100,40) = {}", gcd::gcd(100,40));
+    println!("\nLeaving function driver_gcd");
 }

ff52396185085f07a47fd7a692a1371c65ac894f
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-06-19 05:24:29 +0700

    Test num_complex

---
+++ test_complex/src/main.rs
@@ -0,0 +1,9 @@
+extern crate num_complex;
+
+use num_complex::Complex64;
+
+fn main() {
+    //let c1 = Complex{ re: 1.1, im: 1.2 };
+    let c1 = Complex64 { re: 0.0, im: 0.0 };
+    println!("c1 = {}", c1);
+}

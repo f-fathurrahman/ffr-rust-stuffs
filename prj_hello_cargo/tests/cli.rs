185bde73dd290e9830d780675b1a233a4d46b006
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2022-02-07 04:45:38 +0700

    Try running prj_hello_cargo

--- prj_hello_cargo/tests/cli.rs
+++ prj_hello_cargo/tests/cli.rs
@@ -1,4 +1,8 @@
+use std::process::Command;
+
 #[test]
-fn works() {
-    assert!(true);
+fn runs() {
+    let mut cmd = Command::new("prj_hello_cargo");
+    let res = cmd.output();
+    assert!(res.is_ok());
 }

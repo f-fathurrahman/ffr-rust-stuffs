93b846ddb9369afcc0f035f50539440785f90feb
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-07-21 22:46:07 +0700

    Two examples from "Rust by example"

---
+++ rust_by_example/01_hello_world/hello.rs
@@ -0,0 +1,7 @@
+// This is a comment
+
+fn main() {
+    println!("Hello world from efefer!");
+    println!("I am a Rustacean!");
+}
+
---
+++ rust_by_example/01_hello_world/various_comment.rs
@@ -0,0 +1,20 @@
+fn main() {
+    // An example of a line comment
+
+    /*
+    This is an example of block comment.
+    Another comment
+    */
+
+    let x = 5 + 123;
+    let y = 1.1 + 4.1;
+    println!("This is x: {}", x);
+    println!("And this is y: {}", y);
+
+    // This is not working
+    /*
+    let z = 1.1 + 2;
+    println!("z = {}", z);
+    */
+}
+
